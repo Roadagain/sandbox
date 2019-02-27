e2d1be0afdc68eb1731c37ee0d8b92b636076845
Roadagain <ringoh72@gmail.com>
2019-02-27 20:57:35 +0900

    Basic認証を入れる

--- nuxt-ts-pwa/nuxt.config.ts
+++ nuxt-ts-pwa/nuxt.config.ts
@@ -1,9 +1,17 @@
 export default {
   plugins: ['~/plugins/hello'],
-  modules: ['@nuxtjs/pwa'],
+  modules: [
+    '@nuxtjs/pwa',
+    'nuxt-basic-auth-module'
+  ],
   manifest: {
     name: 'nuxt-ts + pwa sample',
     short_name: 'pwa.nuxt.ts',
     starts_url: '/'
+  },
+  basic: {
+    name: 'ringoh72',
+    pass: 'password',
+    enabled: process.env.BASIC_AUTH_ENABLED === 'true'
   }
 }
