a582e65e0698c41a69b859f3dc96212b810e97fe
Roadagain <ringoh72@gmail.com>
2019-04-09 23:02:13 +0900

    manifestの読み込みを復活させる

--- serverless-next-ts-pwa/src/app/components/Header.tsx
+++ serverless-next-ts-pwa/src/app/components/Header.tsx
@@ -1,7 +1,11 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default ({ pathname }: { pathname?: any }) => (
   <header>
+    <Head>
+      <link rel='manifest' href='/manifest.json' />
+    </Head>
     <Link href='/'>
       <a className={pathname === '/' ? 'is-active' : ''}>Home</a>
     </Link>
